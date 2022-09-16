ace974e4bee92ffc3e4773b16ae7975827dbebce
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-09-16 18:29:34 -0300

    creating type item

--- src/components/App.styles.ts
+++ src/components/App.styles.ts
@@ -14,10 +14,7 @@ background-color: royalblue;
 height: 9.37rem;
 text-align: center;
 
-
 `
-
-
 export const HeaderText = styled.h1`
 margin: 0;
 padding: 0;
@@ -27,5 +24,8 @@ padding-top: 30px;
 
 export const Body = styled.div`
 margin: auto;
+max-width: 980px;
+margin-bottom: 50px;
+
 
 `
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+
+
+export const items = [ 
+    {date: new Date(2021, 9 , 15) , category: 'food', title: 'McDonalds', value: 32.12},
+    {date: new Date(2021, 9 , 21) , category: 'rent', title: 'Aluguel', value: 1500},
+    {date: new Date(2021, 9 , 17) , category: 'food', title: 'Burguer King', value: 27.12},
+    {date: new Date(2021, 10 , 18) , category: 'salary', title: 'Salário ACME', value: 4500}
+]
