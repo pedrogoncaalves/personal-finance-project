83c7339e1a8a642d08f5b5a083de67ed6be6b946
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-09-17 10:15:43 -0300

    ajustes

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,6 +1,6 @@
+import { Item } from '../types/Item'
 
-
-export const items = [ 
+export const items: Item[] = [ 
     {date: new Date(2021, 9 , 15) , category: 'food', title: 'McDonalds', value: 32.12},
     {date: new Date(2021, 9 , 21) , category: 'rent', title: 'Aluguel', value: 1500},
     {date: new Date(2021, 9 , 17) , category: 'food', title: 'Burguer King', value: 27.12},
