879348bfac26858d8f1792fa4fe4adb7dbb25377
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-09-18 19:08:29 -0300

    handling data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import * as C from './components/App.styles'
+import { Category } from './types/Category'
+import { Item } from './types/Item'
+import { categories } from './data/categories'
+import { items } from './data/items'
+import { FilterListByMonth, getCurrentMonth } from './helpers/DateFilter'
+import { TableArea } from './components/TableArea/index'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth)
+
+  useEffect(() => {
+    setFilteredList(FilterListByMonth(list, currentMonth))
+  }, [list, currentMonth]);
+  
 
   return (
     <C.Container>
@@ -11,7 +25,11 @@ function App() {
         <C.HeaderText>Sistema de Controle Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        
+
+
+
+        <TableArea/>
       </C.Body>
     </C.Container>
   )
---
+++ src/components/TableArea/styles.ts
@@ -0,0 +1,18 @@
+
+
+import styled from "styled-components";
+
+
+export const Table = styled.table`
+width: 100%;
+background-color: #FFF;
+padding: 20px;
+box-shadow: 0px 0px 5px #CCC;
+border-radius: 10px;
+
+`
+
+export const TableHeadColumn = styled.table`
+text-align: center;
+
+`
